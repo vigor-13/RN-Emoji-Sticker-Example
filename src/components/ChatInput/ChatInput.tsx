760cf5ef3ff2db7aba0117e48c953683604101f5
vigor13-aladdin <vigor13@buyaladdinrnd.com>
2023-01-09 16:29:11 +0900

    feat: add keyboard change button to input

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View, TouchableOpacity} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const ChatInput = () => {
+  const [isCustomKeyboard, setIsCustomKeyboard] = useState(false);
+
+  const onPressToggleKeyboard = () => {
+    setIsCustomKeyboard(!isCustomKeyboard);
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -10,6 +17,13 @@ const ChatInput = () => {
         placeholderTextColor="#333"
         multiline
       />
+      <TouchableOpacity style={styles.icon} onPress={onPressToggleKeyboard}>
+        <MaterialIcon
+          name={isCustomKeyboard ? 'keyboard' : 'insert-emoticon'}
+          size={24}
+          color="#333"
+        />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -18,6 +32,7 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
     paddingVertical: 10,
+    position: 'relative',
   },
   input: {
     minHeight: 30,
@@ -26,6 +41,12 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingHorizontal: 10,
     color: '#fff',
+    paddingRight: 45,
+  },
+  icon: {
+    position: 'absolute',
+    right: 20,
+    bottom: 13,
   },
 });
 
